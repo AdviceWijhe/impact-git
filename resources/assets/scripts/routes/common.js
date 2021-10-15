57d3bcbfc7e79b07e3143b90a1116cafc61da0a0
AdviceWijhe <49639728+AdviceWijhe@users.noreply.github.com>
2021-10-15 15:03:41 +0200

    updated-header

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,17 +1,50 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
 
-    let searchButton = document.querySelector('.search i');
-    let searchForm = document.querySelector('#searchform')
 
-    searchButton.addEventListener('click', function() {
-      if(searchForm.classList.contains('active')) {
-        searchForm.classList.remove('active');
-      }else {
-        searchForm.classList.add('active');
+    // MOBILE NAVIGATION
+    let fullmenuButton = document.querySelector('.hamburger');
+    let fullmenu = document.querySelector('#fullmenu');
+
+    fullmenuButton.addEventListener('click', function () {
+      if (this.classList.contains('is-active')) {
+        this.classList.remove('is-active');
+        fullmenu.classList.remove('open');
+      } else {
+        this.classList.add('is-active');
+        fullmenu.classList.add('open');
       }
     });
+
+    /** Code By Webdevtrick ( https://webdevtrick.com )  **/
+    $('input').on('focusin', function () {
+      $(this).parent().parent().find('label').addClass('active');
+    });
+
+    $('input').on('focusout', function () {
+      if (!this.value) {
+        $(this).parent().parent().find('label').removeClass('active');
+      }
+    });
+
+    // MOBILE MENU
+    let menuItems = document.querySelectorAll('#fullmenu li.menu-item-has-children > a');
+    // let menuArray = menuItems.prototype.slice.call(nodelist).filter;
+
+    for (let x = 0; x < menuItems.length; x++) {
+      menuItems[x].addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (menuItems[x].parentElement.classList.contains('open')) {
+          menuItems[x].parentElement.classList.remove('open');
+        } else {
+
+          menuItems[x].parentElement.classList.add('open');
+        }
+      });
+    }
+
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,16 @@
 @php
   $logo = get_field("logo", "option");
+  $contactgegevens = get_field("contactgegevens", "option");
 @endphp
 
 <section class="section__header">
-  <nav class="navbar banner d-flex align-items-center justify-content-center">
+
+  {{-- NAV BALK --}}
+  <nav class="navbar banner d-flex justify-content-center">
     <div class="container">
-      <div class="row section__header__row align-items-center">
-        <div class="col-7 col-lg-3">
+      <div class="row align-items-end pt-4">
+        {{-- LOGO --}}
+        <div class="col-8 col-lg-3">
       @if($logo)
           <a class="brand section__header__logo" href="{{ home_url('/') }}"><img src="{{$logo['url']}}" alt="{{ get_bloginfo('name', 'display') }}"></a>
           @else
@@ -14,76 +18,52 @@
           @endif
         </div>
 
+
     {{-- HAMBURGER BUTTON --}}
-        <a class="navbar-toggle collapsed d-lg-none col-5 text-right" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <i class="fas fa-bars section__header__mobileicon"></i>
+    <div class="col-4 text-right d-lg-none hamburger pb-2 pt-3">
+        <a class="navbar-toggle collapsed d-lg-none">
+          <i class="fas fa-align-right section__header__mobileicon default"></i>
+          <i class="fas fa-align-left section__header__mobileicon active"></i>
         </a>
+        <div class="hamburger-background"></div>
+    </div>
 
-    {{-- NAV --}}
-  <div class="col-md-9 d-none d-lg-flex my-auto justify-content-between align-items-center">
-    <div class="nav section__header__text col-md-9">
+      {{-- MOBILE NAVIGATION --}}
+    <div id="fullmenu" class="col-12 d-lg-none">
+
+      <nav class="nav-primary pt-3 pb-5">
         @if (has_nav_menu('primary_navigation'))
-                      <nav class="navbar navbar-expand-md navbar-light" role="navigation">
-  <div class="container justify-content-center">
-        @php
-        wp_nav_menu( array(
-            'theme_location'    => 'primary_navigation',
-            'depth'             => 2,
-            'container'         => 'div',
-            'menu_class'        => 'nav navbar-nav',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new WP_Bootstrap_Navwalker(),
-        ) );
-        @endphp
-    </div>
-</nav>
-            @endif
-
-
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        @endif
+      </nav>
     </div>
 
-        <div class="md-2 section__header__headerbutton">
-      {!! getButton('Bel me terug', '/contact', 'primary') !!}
-        </div>
-        <div class="md-1">
-        {{get_search_form()}}
-        </div>
-  </div>
-</div>
-
-
-    {{-- MOBILE NAV --}}
-  <div id="navbar" class="navbar-collapse collapse section__header__mobilenav">
-    <nav class="nav section__header__text d-flex flex-column section__header__mobilenavitems">
-
+    {{-- DESKTOP NAVIGATION --}}
+  <div class="col-lg-9 d-none d-lg-block" id="desktopmenu">
       @if (has_nav_menu('primary_navigation'))
+        <nav class="navbar navbar-expand-md navbar-light" role="navigation">
+          <div class="container justify-content-center">
+            @php
+            wp_nav_menu( array(
+                'theme_location'    => 'primary_navigation',
+                'depth'             => 2,
+                // 'container'         => 'div',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker(),
+            ) );
+            @endphp
+          </div>
 
-<nav class="navbar-expand-lg" role="navigation">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
 
-        @php
-        wp_nav_menu( array(
-            'theme_location'    => 'primary_navigation',
-            'depth'             => 2,
-            'container'         => 'div',
-            // 'container_class'   => 'collapse navbar-collapse',
-            // 'container_id'      => 'bs-example-navbar-collapse-1',
-            'menu_class'        => 'nav navbar-nav',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new WP_Bootstrap_Navwalker(),
-        ) );
-        @endphp
-    </div>
-</nav>
-
-            @endif
-    </nav>
-
-      {!! getButton('Bel me terug', '/contact', 'primary') !!}
-      {{get_search_form()}}
-
+          </div>
+        </nav>
+      @endif
   </div>
-  </div>
-</nav>
+
+
+
+      {{-- CLOSING --}}
+      </div>
+  </nav>
 </section>
